888ed2f53b3918884c0757861d041883ddfc8e8a
Xavier Giménez <nottu.2584@gmail.com>
2020-07-18 20:15:17 +0200

    app FetchCharacter test

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from '../../images/logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../../images/logo.svg";
+import "./App.css";
+import FetchCharacter from "../../services/api/fetchCharacter";
 
 function App() {
   return (
@@ -10,14 +11,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+        <FetchCharacter></FetchCharacter>
       </header>
     </div>
   );
