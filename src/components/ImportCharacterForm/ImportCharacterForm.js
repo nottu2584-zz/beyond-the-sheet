96b735793322f49d827d7a917d74a9c321b962e3
Pol Santos <polamen02@gmail.com>
2020-07-25 18:55:05 +0200

    component ImportCharacterForm init

---
+++ src/components/ImportCharacterForm/ImportCharacterForm.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+function ImportCharacterForm(props) {
+    const [data, setData] = useState("Import Character Sheet Json")
+
+    const handleChange = (event) => {
+        setData({ value: event.target.value });
+    }
+
+    const handleSubmit = (event) => {
+        alert('An essay was submitted: ' + data);
+        event.preventDefault();
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Essay:
+        <textarea value={data} onChange={handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    );
+    }
+
+
+export default ImportCharacterForm;
+
