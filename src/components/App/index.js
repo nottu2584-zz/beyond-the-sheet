68989980f50697a403a0a8e0b18985aa7e20ea71
Pol Santos <polamen02@gmail.com>
2020-07-31 20:59:49 +0200

    CharacterStats Test

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
 import './App.css';
-//import { CharacterStats } from '../';
+import { CharacterStats } from '../';
 
 function App() {
+
+  const initialStats = {
+    strenght: 10,
+    dexterity: 10,
+    constituion: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +29,25 @@ function App() {
         >
           Learn React
         </a>
+        <CharacterStats
+          strenght="10"
+          dexterity="10"
+          constitution="10"
+          intelligence="10"
+          wisdom="10"
+          charisma="10"
+        ></CharacterStats>
+        <CharacterStats
+          strenght={initialStats.strenght}
+          dexterity={initialStats.dexterity}
+          constitution={initialStats.constituion}
+          intelligence={initialStats.intelligence}
+          wisdom={initialStats.wisdom}
+          charisma={initialStats.charisma}
+        ></CharacterStats>
+        <CharacterStats
+          {...initialStats}
+        ></CharacterStats>
       </header>
     </div>
   );
