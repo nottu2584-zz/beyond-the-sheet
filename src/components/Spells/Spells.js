08939ec60ac5aa68a14da015b86715cabde73191
Xavier Giménez <nottu.2584@gmail.com>
2020-08-11 21:45:30 +0200

    map componentes

--- src/components/Spells/Spells.js
+++ src/components/Spells/Spells.js
@@ -1,43 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Spells = (props) => {
-    const {
-        name,
-        lvl,
-        time,
-        range,
-        hit,
-        DC,
-        duration,
-        components,
-    } = props;
+  const { name, lvl, time, range, hit, DC, duration, components } = props;
 
-    return (
-        <div className="container">
-            <div>Name: {name}</div>
-            <div>Level: {lvl}</div>
-            <div>Time: {time}</div>
-            <div>Range: {range}</div>
-            <div>hit: {hit}</div>
-            <div>DC: {DC}</div>
-            {duration ?
-                <div>Duration: {duration}</div>
-                : null }
-            {components?
-                <div>{props.components.map((component, key) => {
-                return (
-                <div>{component?
-                "V": null}
-                {component?
-                "S": null}
-                {component[3]?
-                "M": null}</div>
-                )
-                
-            })}</div>
-            : "prueba"}    
+  return (
+    <div className="container">
+      <div>Name: {name}</div>
+      <div>Level: {lvl}</div>
+      <div>Time: {time}</div>
+      <div>Range: {range}</div>
+      <div>hit: {hit}</div>
+      <div>DC: {DC}</div>
+      {duration ? <div>Duration: {duration}</div> : null}
+      {components ? (
+        <div>
+          {components.map((component, key) => {
+            switch (component) {
+              case 1:
+                return "V";
+              case 2:
+                return "S";
+              case 3:
+                return "M";
+              default:
+                return null;
+            }
+          })}
         </div>
-    );
+      ) : (
+        "prueba"
+      )}
+    </div>
+  );
 };
 
 export default Spells;
