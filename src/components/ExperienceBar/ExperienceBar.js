f83ffdf0608c4987e893c6692be3874a113f5700
Pol Santos <polamen02@gmail.com>
2020-09-19 20:38:10 +0200

    experience bar init

---
+++ src/components/ExperienceBar/ExperienceBar.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+});
+
+const ExperienceBar = (props) => {
+  const classes = useStyles();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress variant="determinate" value={progress} />
+    </div>
+  );
+}
+
+export default ExperienceBar
