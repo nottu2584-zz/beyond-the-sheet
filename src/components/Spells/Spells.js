d585b96a8c0cd44dc0498a44cae9b5ae4c4315af
Pol Santos <polamen02@gmail.com>
2021-02-08 20:58:03 +0100

    card option deleted

--- src/components/Spells/Spells.js
+++ src/components/Spells/Spells.js
@@ -5,6 +5,7 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Link from "@material-ui/core/Link";
 import React from "react";
 import { SpellCard } from "../SpellCard";
 
@@ -20,97 +21,23 @@ const Spells = (props) => {
     components,
     school,
     avatar,
+    card,
     link = "https://www.dndbeyond.com/spells/",
     characterName,
   } = props;
-  
+
   const generateLink = (name) => {
     const elements = name.split(" ");
     return link + elements.join("-").toLowerCase();
   };
 
   return (
-    <SpellCard
-      avatar={avatar}
-      spellName={name}
-      characterName={characterName}
-      school={school}
-      level={lvl}
-      time={time === 1 ? "1A" : time === 3 ? "1BA" : null}
-      hitDc={
-        hit
-          ? hit
-          : DC
-          ? {
-              1: "STR",
-              2: "DEX",
-              3: "CON",
-              4: "INT",
-              5: "WIS",
-              6: "CHA",
-            }[DC]
-          : "--"
-      }
-      duration={duration ? duration : null}
-      range={range}
-      link={generateLink(name)}
-      components={
-        components
-          ? components.map((component, key) => {
-              switch (component) {
-                case 1:
-                  return "V";
-                case 2:
-                  return "S";
-                case 3:
-                  return "M";
-                default:
-                  return null;
-              }
-            })
-          : null
-      }
-    ></SpellCard>
+    <li>
+      <Link href={generateLink(name)} variant="body2" color="inherit">
+        {name}
+      </Link>
+    </li>
   );
-  //   <TableRow>
-  //     <TableCell></TableCell>
-  //     <TableCell>{name}</TableCell>
-  //     <TableCell>{lvl}</TableCell>
-  //     <TableCell>{time === 1 ? "1A" : time === 3 ? "1BA" : null}</TableCell>
-  //     <TableCell>{range}</TableCell>
-  //     <TableCell>
-  //       {hit
-  //         ? hit
-  //         : DC
-  //         ? {
-  //             1: "STR",
-  //             2: "DEX",
-  //             3: "CON",
-  //             4: "INT",
-  //             5: "WIS",
-  //             6: "CHA",
-  //           }[DC]
-  //         : "--"}
-  //     </TableCell>
-  //     <TableCell>{duration ? duration : null}</TableCell>
-  //     <TableCell>
-  //       {components
-  //         ? components.map((component, key) => {
-  //             switch (component) {
-  //               case 1:
-  //                 return "V";
-  //               case 2:
-  //                 return "S";
-  //               case 3:
-  //                 return "M";
-  //               default:
-  //                 return null;
-  //             }
-  //           })
-  //         : null}
-  //     </TableCell>
-  //   </TableRow>
-  // );
 };
 
 export default Spells;
