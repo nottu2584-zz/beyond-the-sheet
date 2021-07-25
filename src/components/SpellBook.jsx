d3fa80c8a2ca01df8d2e393dda3f2a512f12158b
Pol Santos <polamen02@gmail.com>
2021-07-25 14:43:23 +0200

    spellSlots in progress

--- src/components/SpellBook.jsx
+++ src/components/SpellBook.jsx
@@ -23,15 +23,40 @@ const Levels = () => {
   return { levels: new Array(10) };
 };
 
+const spellCasterSlots = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [2, 0, 0, 0, 0, 0, 0, 0, 0],
+  [3, 0, 0, 0, 0, 0, 0, 0, 0],
+  [4, 2, 0, 0, 0, 0, 0, 0, 0],
+  [4, 3, 0, 0, 0, 0, 0, 0, 0],
+  [4, 3, 2, 0, 0, 0, 0, 0, 0],
+  [4, 3, 3, 0, 0, 0, 0, 0, 0],
+  [4, 3, 3, 1, 0, 0, 0, 0, 0],
+  [4, 3, 3, 2, 0, 0, 0, 0, 0],
+  [4, 3, 3, 3, 1, 0, 0, 0, 0],
+  [4, 3, 3, 3, 2, 0, 0, 0, 0],
+  [4, 3, 3, 3, 2, 1, 0, 0, 0],
+  [4, 3, 3, 3, 2, 1, 0, 0, 0],
+  [4, 3, 3, 3, 2, 1, 1, 0, 0],
+  [4, 3, 3, 3, 2, 1, 1, 0, 0],
+  [4, 3, 3, 3, 2, 1, 1, 1, 0],
+  [4, 3, 3, 3, 2, 1, 1, 1, 0],
+  [4, 3, 3, 3, 2, 1, 1, 1, 1],
+  [4, 3, 3, 3, 3, 1, 1, 1, 1],
+  [4, 3, 3, 3, 3, 2, 1, 1, 1],
+  [4, 3, 3, 3, 3, 2, 2, 1, 1],
+];
+
 const SpellBook = (props) => {
   const [toggle, setToggle] = useState(false);
+  const classes = useStyles();
+  const { characters } = props;
+
+  if (!characters.length) return null;
 
   const handleChange = (event) => {
     setToggle(!toggle);
   };
-
-  const classes = useStyles();
-  const { characters } = props;
 
   //TODO: ViewCharactersByLevel && ViewSpellsByLevel
   const SpellKeeper = (characters) => {
@@ -39,9 +64,64 @@ const SpellBook = (props) => {
     const ViewSpellsByLevel = Levels();
 
     for (let character of characters) {
-      const { avatarUrl, id, name, classSpells, spells } = character.data;
+      const {
+        avatarUrl,
+        classes,
+        id,
+        name,
+        classSpells,
+        spells,
+        spellSlots,
+        pactMagic,
+      } = character.data;
 
-      for (let key = 0; key < 10; key++) {
+      //TODO: class slots && multiclass
+      character.spellCasting = {
+        isSpellCaster: { value: false, level: 0 },
+        isHalfCaster: { value: false, level: 0 },
+        isPactMagic: { value: false, level: 0 },
+      };
+      //TODO: 1r Spellcaster lvl de cada character (comprobar multiclass). 2nd SpellSlots "halfCaster, etc." (sacado de el json)
+      // for (let characterClass of classes) {
+      //   if (isSpellCaster(characterClass)) {
+      //     character.spellCasting.isSpellCaster.value = true;
+      //     character.spellCasting.isSpellCaster.level =
+      //       character.spellCasting.isSpellCaster.level + characterClass.level;
+      //   }
+      //   if (isHalfCaster(characterClass)) {
+      //     character.spellCasting.isHalfCaster.value = true;
+      //     character.spellCasting.isHalfCaster.level =
+      //       character.spellCasting.isHalfCaster.level + characterClass.level;
+      //   }
+      //   if (isPactMagic(characterClass)) {
+      //     character.spellCasting.isPactMagic.value = true;
+      //     character.spellCasting.isPactMagic.level =
+      //       character.spellCasting.isPactMagic.level + characterClass.level;
+      //   }
+      // }
+
+      // console.log(
+      //   "Suma Levels",
+      //   character.spellCasting.isHalfCaster.level +
+      //     character.spellCasting.isSpellCaster.level
+      // );
+
+      // const CharacterSpellCasterSlots =
+      //   character.spellCasting.isSpellCaster.value &&
+      //   character.spellCasting.isHalfCaster.value
+      //     ? spellCasterSlots[
+      //         character.spellCasting.isSpellCaster.level +
+      //           Math.floor(character.spellCasting.isHalfCaster.level / 2)
+      //       ]
+      //     : character.spellCasting.isSpellCaster.value
+      //     ? spellCasterSlots[character.spellCasting.isSpellCaster.level]
+      //     : character.spellCasting.isHalfCaster.value
+      //     ? spellCasterSlots[character.spellCasting.isHalfCaster.level - 2]
+      //     : null;
+
+
+      for (let key = 0; key < "spellCasterSlots[spellCasterLevel]"; key++) {
+        if (key > 0 && "spellCasterSlots[spellCasterLevel][key]" === 0) break;
         if (!Array.isArray(ViewCharactersByLevel.levels[key]))
           ViewCharactersByLevel.levels[key] = [];
 
@@ -77,9 +157,28 @@ const SpellBook = (props) => {
                 spellsItem,
                 spellsRace
               )
-              .filter((spell) => spell != null)
+              .filter((spell) => spell !== null)
           ),
+          // levelSpellSlots: key > 0 ? CharacterspellCasterSlots[key - 1] : null,
+          // levelSpendSlots: key > 0 ? spellSlots[key - 1].used : null,
+          // levelPactMagicSlots:
+          //   character.spellCasting.isPactMagic.value && key > 0
+          //     ? PactMagicSlots[character.spellCasting.isPactMagic.level][
+          //         key - 1
+          //       ] !== 0
+          //       ? PactMagicSlots[character.spellCasting.isPactMagic.level][
+          //           key - 1
+          //         ]
+          //       : null
+          //     : null,
+          // levelSpendPactMagicSlots:
+          //   key > 0 && key < 6 ? pactMagic[key - 1].used : null,
         };
+        // if (
+        //   key === 0 ||
+        //   characterLevel.levelSpellSlots ||
+        //   characterLevel.levelPactMagicSlots
+        // )
 
         characterLevel.level.length &&
           ViewCharactersByLevel.levels[key].push(characterLevel);
@@ -97,7 +196,7 @@ const SpellBook = (props) => {
               spellsItem,
               spellsRace
             )
-            .filter((spell) => spell != null)
+            .filter((spell) => spell != null && spell.definition.level === key)
         );
 
         ViewSpellsByLevel.levels[key] = ViewSpellsByLevel.levels[key]
@@ -110,9 +209,13 @@ const SpellBook = (props) => {
 
   const getSpells = (spellBook, key) => {
     if (Array.isArray(spellBook) && spellBook.length) {
-      return spellBook.filter((spell) => {
-        return spell.definition.level === key;
-      });
+      return spellBook.filter(
+        (spell) =>
+          spell.definition.level === key ||
+          (spell.definition.scaleType &&
+            spell.definition.level !== 0 &&
+            key !== 0)
+      );
     } else return null;
   };
 
@@ -141,6 +244,35 @@ const SpellBook = (props) => {
     } else return null;
   };
 
+  const isSpellCaster = (characterClass) => {
+    const { name } = characterClass.definition;
+    return (
+      name === "Artificer" ||
+      name === "Bard" ||
+      name === "Cleric" ||
+      name === "Druid" ||
+      name === "Sorcerer" ||
+      name === "Wizard"
+    );
+  };
+
+  const isHalfCaster = (characterClass) => {
+    const { name } = characterClass.definition;
+    return (
+      name === "Paladin" ||
+      name === "Ranger" ||
+      (name === "Fighter" &&
+        characterClass.subclassDefinition?.name === "Eldritch Knight") ||
+      (name === "Rogue" &&
+        characterClass.subclassDefinition?.name === "Arcane Trickster")
+    );
+  };
+
+  const isPactMagic = (characterClass) => {
+    const { name } = characterClass.definition;
+    return name === "Warlock";
+  };
+
   console.log("SpellKeeper", SpellKeeper(characters));
 
   const generateLink = (name) => {
@@ -154,6 +286,7 @@ const SpellBook = (props) => {
         .toLowerCase()
     );
   };
+
   return (
     <>
       <Switch onChange={handleChange} color="default"></Switch>
